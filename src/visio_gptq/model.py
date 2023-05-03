b33dd7590250c3904a303b4e60b6ff3de7f260fe
Marco João <marco.joao@gaspardbruno.com>
2023-05-03 16:09:20 +0100

    update model and examples

--- src/visio_gptq/model.py
+++ src/visio_gptq/model.py
@@ -1,5 +1,6 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
+import gc
 from transformers import  AutoModelForCausalLM
 from visio_gptq.conversation import Conversation, SeparatorStyle
 from visio_gptq.quad import load_quantized
@@ -61,41 +62,77 @@ class GPTQModel:
 
         return tokenizer, model, context_len
 
+
     @torch.inference_mode()
     def __generate_stream(self, prompt:str, **kwargs):
 
-        l_prompt = len(prompt)
-        temperature = float(kwargs.get("temperature") or 0)
-        max_new_tokens = int(kwargs.get("max_new_tokens") or 512)
+        len_prompt = len(prompt)
+        temperature = float(kwargs.get("temperature") or 0.7)
+        max_new_tokens = int(kwargs.get("max_new_tokens") or 256)
         context_len = int(kwargs.get("context_len") or 2048)
         stream_interval = int(kwargs.get("stream_interval") or 2)
         stop_array = kwargs.get("stop") or ["###"]
+        echo = kwargs.get("echo") or True
+        stop_token_ids = kwargs.get("stop_token_ids", []) or []
+        stop_token_ids.append(self.tokenizer.eos_token_id)
 
         input_ids = self.tokenizer(prompt).input_ids
+        input_echo_len = len(input_ids)
         output_ids = list(input_ids)
 
-        max_src_len = context_len - max_new_tokens - 8
+        if self.model.config.is_encoder_decoder:
+            max_src_len = context_len
+        else:
+            max_src_len = context_len - max_new_tokens - 8
+
         input_ids = input_ids[-max_src_len:]
-        past_key_values = None
+        start_ids = None
+        encoder_output = None
         token = None
+        past_key_values = None
+        out = None
+        if self.model.config.is_encoder_decoder:
+            encoder_output = self.model.encoder(input_ids=torch.as_tensor([input_ids],
+                                                        device=self.device))[0]
+            start_ids = torch.as_tensor([[self.model.generation_config.decoder_start_token_id]],
+                        dtype=torch.int64, device=self.device)
+
         for i in range(max_new_tokens):
             if i == 0:
-                out = self.model(torch.as_tensor([input_ids], device=self.device), use_cache=True)
-                logits = out.logits
+                if self.model.config.is_encoder_decoder:
+                    out = self.model.decoder(input_ids=start_ids,
+                                        encoder_hidden_states=encoder_output,
+                                        use_cache=True)
+                    logits = self.model.lm_head(out[0])
+                else:
+                    out = self.model(torch.as_tensor([input_ids], device=self.device), use_cache=True)
+                    logits = out.logits
                 past_key_values = out.past_key_values
             else:
-                if past_key_values is None:
-                    raise ValueError("past_key_values is None")
-                attention_mask = torch.ones(
-                    1, past_key_values[0][0].shape[-2] + 1, device=self.device)
-                out = self.model(input_ids=torch.as_tensor([[token]], device=self.device),
-                            use_cache=True,
-                            attention_mask=attention_mask,
-                            past_key_values=past_key_values)
-                logits = out.logits
+                if self.model.config.is_encoder_decoder:
+                    out = self.model.decoder(input_ids=torch.as_tensor([[token]],
+                                            device=self.device),
+                                             encoder_hidden_states=encoder_output,
+                                             use_cache=True,
+                                             past_key_values=past_key_values)
+
+                    logits = self.model.lm_head(out[0])
+                else:
+                    out = self.model(
+                        input_ids=torch.as_tensor([[token]],
+                        device=self.device),
+                        use_cache=True,
+                        past_key_values=past_key_values,
+                    )
+                    logits = out.logits
                 past_key_values = out.past_key_values
 
             last_token_logits = logits[0][-1]
+
+            if self.device == "mps":
+                # Switch to CPU by avoiding some bugs in mps backend.
+                last_token_logits = last_token_logits.float().to("cpu")
+
             if temperature < 1e-4:
                 token = int(torch.argmax(last_token_logits))
             else:
@@ -104,27 +141,36 @@ class GPTQModel:
 
             output_ids.append(token)
 
-            if token == self.tokenizer.eos_token_id:
+            if token in stop_token_ids:
                 stopped = True
             else:
                 stopped = False
 
             if i % stream_interval == 0 or i == max_new_tokens - 1 or stopped:
-                output = self.tokenizer.decode(output_ids, skip_special_tokens=True)
+                if echo:
+                    tmp_output_ids = output_ids
+                    rfind_start = len_prompt
+                else:
+                    tmp_output_ids = output_ids[input_echo_len:]
+                    rfind_start = 0
+
+                output = self.tokenizer.decode(tmp_output_ids, skip_special_tokens=True, 
+                                        spaces_between_special_tokens=False)
                 for stop_str in stop_array:
-                    pos = output.rfind(stop_str, l_prompt)
+                    pos = output.rfind(stop_str, rfind_start)
                     if pos != -1:
                         output = output[:pos]
                         stopped = True
-                        break
                 yield output
 
             if stopped:
                 break
 
-        del past_key_values
+        del past_key_values, out
+        gc.collect()
+        torch.cuda.empty_cache()
 
-    def inference(self, prompt: str, **kwargs):
+    def generate_stream(self, prompt: str, **kwargs):
         conversation = kwargs.get("conversation", None)
         if conversation is not None:
             if not isinstance(conversation, Conversation):
@@ -136,22 +182,30 @@ class GPTQModel:
             prompt = new_prompt
             kwargs["stop"] = conversation.sep if conversation.sep_style == SeparatorStyle.SINGLE else conversation.sep2
 
-        stream_callback = kwargs.get("stream_callback", None)
         outputs = []
         pre = 0
         for outputs in self.__generate_stream(prompt, **kwargs):
             outputs = outputs[len(prompt) + 1:].strip()
             outputs = outputs.split(" ")
-            
+
+            now = len(outputs) - 1
+            if now > pre:
+                data = " ".join(outputs[pre:now])
+                yield data
+                pre = now
+        yield " ".join(outputs[pre:])
+
+    def generate(self, prompt: str, **kwargs):
+        generator = self.generate_stream(prompt=prompt, **kwargs)
+        stream_callback = kwargs.get("stream_callback", None)
+        conversation = kwargs.get("conversation", None)
+        output = ""
+        for result in generator:
+            output += " " + result
             if stream_callback:
-                now = len(outputs) - 1
-                if now > pre:
-                    stream_callback(" ".join(outputs[pre:now]))
-                    pre = now
-                
-        if stream_callback:
-            stream_callback(" ".join(outputs[pre:]))
+                stream_callback(result)
+
         if conversation is not None:
-            conversation.messages[-1][-1] = " ".join(outputs)
+            conversation.messages[-1][-1] = output
             return conversation
-        return " ".join(outputs)
+        return output
